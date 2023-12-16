cce5b3a95759b8f4b422125c8a6ba5271bef39ea
Mayur <kssmsslss330@gmail.com>
2023-12-16 11:32:51 +0530

    error resolve  commit

--- next.config.js
+++ next.config.js
@@ -1,26 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      'lh3.googleusercontent.com',
-      'res.cloudinary.com'
-    ]
-  },
-  env: {
-    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: "dnxie3elk",
-    NEXT_PUBLIC_CLOUDINARY_PRESET_NAME:"tirlnr89"
-  },
   experimental: {
     appDir: true,
   },
- 
-
+  images: {
+    domains: [
+      'res.cloudinary.com', 
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com'
+    ]
+  }
 }
- 
 
-module.exports = {
-
-  nextConfig
-
-  
-}
+module.exports = nextConfig
